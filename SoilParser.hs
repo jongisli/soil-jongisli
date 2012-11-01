692a57f49a22d8428ce3ff5ad2c79e0240931292
Jón Gísli Egilsson <jongisli@Jons-MacBook-Pro.local>
2012-11-01 12:18:14 +0100

    working pretty well, next up is Cases

--- SoilParser.hs
+++ SoilParser.hs
@@ -9,6 +9,10 @@ import SimpleParse
 import SoilAst
 import Data.Char(isSpace,isLower,isUpper,isDigit,isAlpha,isAlphaNum)
 
+keyWords = ["self", "to", "create", "with", "become", 
+            "let", "from", "case", "send", "concat",
+            "if", "then", "else"]
+
 digit :: Parser Char
 digit = satisfy isDigit
 
@@ -19,24 +23,39 @@ nameRest :: Parser Char
 nameRest = satisfy isAlpha <|> satisfy isDigit <|> satisfy (\x -> x == '_')
 
 name :: Parser Name
-name = token $ do c <- letter -- token might let it hang
-                  cs <- many nameRest
-                  return (c:cs)
+name = do c <- letter
+          cs <- many nameRest
+          if (c:cs `elem` keyWords)
+             then reject
+             else return (c:cs)
 
 ident :: Parser Ident
-ident = token $ do c <- satisfy (\x -> x == '#') -- token might let it hang
-                   cs <- name
-                   return cs
+ident = do c <- satisfy (\x -> x == '#')
+           cs <- name
+           return cs
 
-prim :: Parser Prim
-prim = token $ self <|> name' <|> ident'
+-- A Prim parser that doesn't allow concatenation
+-- used in the Prim parser below to avoid loops when
+-- concatenating.
+prim' :: Parser Prim
+prim' = token $ self <|> name' <|> ident'
        where self = do symbol "self"
                        return Self
              ident' = do i <- ident
                          return (Id i)
              name' = do n <- name
                         return (Par n)
-            
+
+prim :: Parser Prim
+prim = token $ self <|> name' <|> ident' <|> concat
+       where self = do symbol "self"
+                       return Self
+             ident' = do i <- ident
+                         return (Id i)
+             name' = do n <- name
+                        return (Par n)
+             concat = chainl1 prim' (symbol "concat" >> return Concat)
+             -- expr = chainl1 prim (char ’+’ >> return Add)
 
 someArgs :: Parser [Prim]
 someArgs = token $ do p  <- prim `sepBy1` (schar ',')
@@ -73,6 +92,9 @@ actop = token $ sendTo <|> create <|> become
                           (a,as) <- fcall
                           return (Become a as)
 
+actops :: Parser [ActOp]
+actops = token $ many actop
+
 
 
 
@@ -80,7 +102,13 @@ actop = token $ sendTo <|> create <|> become
 parseString :: String -> Either Error Program
 parseString s = Left "Error bro"
 
-testTxt = "send (#ok) to self"
+-- actop test
+testTxt1 = "send (#ok) to self"
+-- actops test
+testTxt2 = "send (#ok) to self create jon with gisli(yo,yo)"
+-- keyword test: should fail
+testTxt3 = "send (#create) to self"
+
 -- Right ([], [SendTo [Id "ok"] Self])
 
 --parseFile :: FilePath -> IO (Either Error Program)
